c5c50659d04278bafa243d8b4c9c2c941f14eab3
Julia <juliasantosmarchi01@gmail.com>
2020-11-26 14:25:40 -0300

    commit inicial

---
+++ app/Controllers/Http/PessoasController.ts
@@ -0,0 +1,19 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PessoasController {
+  public async index({ view }: HttpContextContract) {
+    return view.render('pessoa/index', { message: 'ola galera' })
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
