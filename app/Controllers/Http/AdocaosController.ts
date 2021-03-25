393f11e2deb83dbd0d759a86dfd871dc5718c3f9
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-03-25 11:08:03 -0300

    query do match

--- app/Controllers/Http/AdocaosController.ts
+++ app/Controllers/Http/AdocaosController.ts
@@ -2,15 +2,26 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Adocao from 'App/Models/Adocao'
 import Doacao from 'App/Models/Doacao'
+import Animal from 'App/Models/Animal'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class AdocaosController {
   public async index ({}: HttpContextContract) {
   }
 
   public async create ({}: HttpContextContract) {
-    const doacaoAtiva = await Doacao.query()
-      .where('ativo', true)
-    //const adocao = new Adocao();
+    
+    const res = Database.rawQuery("select ac.animal_id from animal_caracteristica as ac inner join pessoa_caracteristica as pc on ac.caracteristica_id = pc.caracteristica_id inner join doacaos as d on d.animal_id=ac.animal_id where pc.pessoa_id=4 and d.ativo=true")
+    const animaisMatch = []
+
+    for(const animal_id in res){
+        const animal = await Animal.find(animal_id)
+        animaisMatch.push(animal)
+    }
+
+    console.log(animaisMatch)
+
+    //select animal_id from animal_caracteristica as ac inner join pessoa_caracteristica as pc on ac.caracteristica_id = pc.caracteristica_id where pc.pessoa_id = 4;
   }
 
   public async store ({}: HttpContextContract) {
---
+++ database/migrations/1616678970640_animais_caracteristicas.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class AnimalCaracteristicas extends BaseSchema {
+  protected tableName = 'animal_caracteristica'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.integer('animal_id').unsigned().references('id').inTable('animals')
+      table.integer('caracteristica_id').unsigned().references('id').inTable('caracteristicas')
+      
+      table.timestamps(true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
